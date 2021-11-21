b22f24d1972225f97a0734ea109658013c06c035
mrago256 <mrago256@gmail.com>
2021-11-20 22:50:59 -0500

    Added full functionality

--- AutoScreener.py
+++ AutoScreener.py
@@ -44,7 +44,7 @@ chrome_options = Options()
 chrome_options.add_argument("--headless")
 
 try:
-  driver = webdriver.Chrome("./chromedriver")#, options=chrome_options)
+  driver = webdriver.Chrome("./chromedriver", options=chrome_options)
 except selenium.common.exceptions.WebDriverException:
   print("Driver file not found. Please refer to readme")
   exit()
@@ -85,6 +85,6 @@ driver.find_element_by_id("f_251742_no").click()
 driver.find_element_by_id("f_255927_no").click()
 
 # click submit
-# driver.find_element_by_id("formy-button").click()
+driver.find_element_by_id("formy-button").click()
 
-# driver.close()
+driver.close()
