aa9547f7fed441220ecec329bd0f2115afd025bd
mrago256 <mrago256@gmail.com>
2022-02-10 13:39:20 -0500

    updater.sh functional

--- updater.sh
+++ updater.sh
@@ -3,4 +3,10 @@
 version=`google-chrome --version | cut -d ' ' -f3`
 echo $version
 
-wget https://chromedriver.storage.googleapis.com/$version/chromedriver_linux64.zip
+wget -N https://chromedriver.storage.googleapis.com/$version/chromedriver_linux64.zip
+
+unzip -o *.zip
+
+rm *.zip
+
+echo ChromeDriver version successfully matched with chrome version: $version
